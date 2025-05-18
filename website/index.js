6a00e75bdcc1477d5e284a65028c17de2b4007ca
unknown <azklanxpaul09@gmail.com>
2025-05-18 20:14:27 +0800

    new upd

--- website/index.js
+++ website/index.js
@@ -7,3 +7,22 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+let first = true;
+
+function clickme(){
+    const textElement = document.getElementById("wel");
+    const textElement2 = document.getElementById("btn");
+    const textElement3 = document.getElementById("p1");
+    if(first){
+        textElement.textContent = "Goodboy!";
+        textElement2.textContent = "Click me again!";
+        textElement3.textContent = "You are such a GOODBOY!";
+    } else{
+        textElement.textContent = "Welcome!";
+        textElement2.textContent = "Click me!";
+        textElement3.textContent = "this website is for my project";
+    }
+    first = !first;
+}
+
